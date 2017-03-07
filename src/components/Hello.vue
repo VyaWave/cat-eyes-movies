a2c01a74a8a067758df3d0c5cce6bed8c06a160a
Weiya <weiya@teambition.com>
2017-03-07 23:42:58 +0800

    Revert "dbftest2"
    This reverts commit 0ed585f96346f2fc10c2f18baba2e17369d60778.

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -12,6 +12,7 @@
     </ul>
     <h2>Ecosystem</h2>
     <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-dbffffff</a></li>
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>

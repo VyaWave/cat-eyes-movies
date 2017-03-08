845e080c04e3cbbeb9d5a71fadeda48044dc14ac
Weiya <weiya@teambition.com>
2017-03-08 13:43:34 +0800

    调整结构

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -9,7 +9,7 @@ function resolve (dir) {
 
 module.exports = {
   entry: {
-    app: './src/main.js'
+    app: './src/index.js'
   },
   output: {
     path: config.build.assetsRoot,
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './app/App'
 import router from './router'
+// import Component from 'vue-class-component'
+
+/**
+ * 此处为入口文件
+ */
 
 Vue.config.productionTip = false
 

6797d2f9b231a9d15955c501b3104d3da9b59518
Wave <372969383@qq.com>
2017-03-19 21:24:18 +0800

    首页热映电影

--- src/views/movies/index.js
+++ src/views/movies/index.js
@@ -2,7 +2,7 @@ import Component from 'vue-class-component'
 import Vue from 'Vue'
 import './style.scss'
 import {mapState, mapActions} from 'vuex'
-import { moviesComing } from './mock.movies'
+import { moviesHotData } from './mock.movies'
 import { cities } from './mock.cities'
 
 @Component({
@@ -24,10 +24,8 @@ import { cities } from './mock.cities'
 export default class MoviesList extends Vue {
   created () {
     // 电影数据和城市数据
-    console.log(moviesComing, cities)
-    this.getlist()
-  }
-  bindEvent () {
-    console.log('绑定方法写在这里')
+    // this.getlist()
+    this.movies = moviesHotData.data.movies
+    this.cities = cities
   }
 }

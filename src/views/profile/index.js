fba8283e49dd5db53ddfc174b43c91cf0c93089b
Wave <372969383@qq.com>
2017-04-16 23:26:56 +0800

    loading

--- src/views/profile/index.js
+++ src/views/profile/index.js
@@ -1,15 +1,23 @@
 import Component from 'vue-class-component'
 import{ BaseView } from '../base.view'
 import './style.scss'
-// import { cinemas } from './mock.ciemas'
+import { LoadingIndicator } from '../../components/loading-indicator/index'
 
 @Component({
-  template: require('./template.html')
+  template: require('./template.html'),
+  components:{LoadingIndicator},
+  data: () => ({
+    isLoading: Boolean
+  })
 })
 
 export default class CinemasView extends BaseView {
 
   created () {
      this.headerConfig  = {title: '我的'}
+     this.isLoading = true
+     setTimeout(() => {
+       this.isLoading = false
+     }, 1000 * 0.8)
   }
 }

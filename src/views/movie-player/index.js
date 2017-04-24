78819bc89151dbc56d6568e23f9f1a238abef7e7
Wave <372969383@qq.com>
2017-04-24 10:35:53 +0800

    完善播放页信息

--- src/views/movie-player/index.js
+++ src/views/movie-player/index.js
@@ -3,20 +3,26 @@ import{ BaseView } from '../base.view'
 import { Navbar } from '../navbar/index'
 import VueDPlayer from 'vue-dplayer'
 import { videos } from './mock.videos'
+import { LoadingIndicator } from '../../components/loading-indicator/index'
 import './style.scss'
 
 @Component({
   template: require('./template.html'),
   components: {
-    'd-player': VueDPlayer
+    'd-player': VueDPlayer,
+    LoadingIndicator
   },
   data: () => ({
     video: {
       url: 'http://vodcdn.video.taobao.com/oss/taobao-ugc/fb4821e7e9af423b95c52811f4c3a966/1480556533/video.mp4',
       pic: 'https://gw.alicdn.com/i2/TB1CP.5NXXXXXawXXXXXXXXXXXX_.jpg',
-      type: 'auto'
+      type: 'auto',
+      post: 'https://gw.alicdn.com/i4/TB1onyyOXXXXXcrapXXXXXXXXXX_.jpg',
+      name: '你的名字',
+      msg: '观看： 9000000+'
     },
-    videos: null
+    videos: null,
+    isLoading: Boolean
   }),
 })
 
@@ -31,9 +37,14 @@ export default class MoviePlayer extends BaseView {
       loop: false
     })
     this.videos = videos
+    this.isLoading = true
+    setTimeout(()=> {
+      this.isLoading = false
+    }, 3 * 1000)
   }
 
   playIt(item) {
+    this.video = item
     this.player.switchVideo({
       url: item.url,
       pic: item.pic,
--- src/views/movie-player/mock.videos.js
+++ src/views/movie-player/mock.videos.js
@@ -1,7 +1,7 @@
 export const videos = [{
     url: 'http://cloud.video.taobao.com/play/u/1745440806/p/1/e/6/t/1/d/sd/45490746.mp4',
     post: 'https://gw.alicdn.com/i4/TB1lHp_OXXXXXcCXFXXXXXXXXXX_.jpg',
-    pic: 'https://gw.alicdn.com/i2/TB1CP.5NXXXXXawXXXXXXXXXXXX_.jpg',
+    pic: '',
     type: 'auto',
     name: '神奇动物在哪里',
     msg: '观看： 300000+'
@@ -9,14 +9,14 @@ export const videos = [{
   {
     url: 'http://cloud.video.taobao.com/play/u/1745440806/p/1/e/6/t/1/d/hd/48216103.mp4',
     post: 'https://gw.alicdn.com/i4/TB18r0eOpXXXXa9apXXXXXXXXXX_.jpg',
-    pic: 'https://gw.alicdn.com/i2/TB1CP.5NXXXXXawXXXXXXXXXXXX_.jpg',
+    pic: '',
     type: 'auto',
     name: '血战钢铁岭',
     msg: '观看： 500000+'
   },{
     url: 'http://cloud.video.taobao.com/play/u/1745440806/p/1/e/6/t/1/d/hd/45862110.mp4',
     post: 'https://gw.alicdn.com/i4/TB1FCxZOXXXXXXcaXXXXXXXXXXX_.jpg',
-    pic: 'https://gw.alicdn.com/i2/TB1CP.5NXXXXXawXXXXXXXXXXXX_.jpg',
+    pic: '',
     type: 'auto',
     name: '长城',
     msg: '观看： 600000+'
@@ -24,14 +24,14 @@ export const videos = [{
   {
     url: 'http://cloud.video.taobao.com/play/u/1745440806/p/1/e/6/t/1/d/hd/47763460.mp4',
     post: 'https://gw.alicdn.com/i3/TB1wwcCOpXXXXaCXXXXXXXXXXXX_.jpg',
-    pic: 'https://gw.alicdn.com/i2/TB1CP.5NXXXXXawXXXXXXXXXXXX_.jpg',
+    pic: '',
     type: 'auto',
     name: '28岁未成年',
     msg: '观看： 900000000+'
   },{
     url: 'http://cloud.video.taobao.com/play/u/1745440806/p/1/e/6/t/1/d/hd/47706109.mp4',
     post: 'https://gw.alicdn.com/i4/TB1lTZdNVXXXXXSXXXXXXXXXXXX_.jpg',
-    pic: 'https://gw.alicdn.com/i2/TB1CP.5NXXXXXawXXXXXXXXXXXX_.jpg',
+    pic: '',
     type: 'auto',
     name: '奇幻城堡',
     msg: '观看： 1000000+'
@@ -39,14 +39,14 @@ export const videos = [{
   {
     url: 'http://cloud.video.taobao.com/play/u/1745440806/p/1/e/6/t/1/d/hd/46775974.mp4',
     post: 'https://gw.alicdn.com/i4/TB1oCtGOXXXXXaZXVXXXXXXXXXX_.jpg',
-    pic: 'https://gw.alicdn.com/i2/TB1CP.5NXXXXXawXXXXXXXXXXXX_.jpg',
+    pic: '',
     type: 'auto',
     name: '海洋奇缘',
     msg: '观看： 1200000+'
   },{
     url: 'http://cloud.video.taobao.com/play/u/1745440806/p/1/e/6/t/1/d/sd/47607495.mp4',
     post: 'https://gw.alicdn.com/i4/TB1w1K3OpXXXXatapXXXXXXXXXX_.jpg',
-    pic: 'https://gw.alicdn.com/i2/TB1CP.5NXXXXXawXXXXXXXXXXXX_.jpg',
+    pic: '',
     type: 'auto',
     name: '超级快递',
     msg: '观看： 22200000+'
@@ -54,14 +54,14 @@ export const videos = [{
   {
     url: 'http://cloud.video.taobao.com/play/u/1745440806/p/1/e/6/t/1/d/hd/47395036.mp4',
     post: 'https://gw.alicdn.com/i3/TB1a42mOXXXXXcRXpXXXXXXXXXX_.jpg',
-    pic: 'https://gw.alicdn.com/i2/TB1CP.5NXXXXXawXXXXXXXXXXXX_.jpg',
+    pic: '',
     type: 'auto',
     name: '爱上试睡师',
     msg: '观看： 2300000+'
   },{
     url: 'http://cloud.video.taobao.com/play/u/1745440806/p/1/e/6/t/1/d/sd/47466383.mp4',
     post: 'https://gw.alicdn.com/i4/TB18tU8OXXXXXXAaXXXXXXXXXXX_.jpg',
-    pic: 'https://gw.alicdn.com/i2/TB1CP.5NXXXXXawXXXXXXXXXXXX_.jpg',
+    pic: '',
     type: 'auto',
     name: '夏威夷之恋',
     msg: '观看： 29000000+'
@@ -69,14 +69,14 @@ export const videos = [{
   {
     url: 'http://cloud.video.taobao.com/play/u/1745440806/p/1/e/6/t/1/d/hd/46982677.mp4',
     post: 'https://gw.alicdn.com/i1/TB1.7vmOXXXXXaKXpXXXXXXXXXX_.jpg',
-    pic: 'https://gw.alicdn.com/i2/TB1CP.5NXXXXXawXXXXXXXXXXXX_.jpg',
+    pic: '',
     type: 'auto',
     name: '我在故宫修文物',
     msg: '观看： 87900000+'
   },{
     url: 'http://cloud.video.taobao.com/play/u/1745440806/p/1/e/6/t/1/d/hd/45856368.mp4',
     post: 'https://gw.alicdn.com/i3/TB1qw1PNFXXXXXRapXXXXXXXXXX_.jpg',
-    pic: 'https://gw.alicdn.com/i2/TB1CP.5NXXXXXawXXXXXXXXXXXX_.jpg',
+    pic: '',
     type: 'auto',
     name: '魲得水',
     msg: '观看： 9000000+'
@@ -84,14 +84,14 @@ export const videos = [{
   {
     url: 'http://cloud.video.taobao.com/play/u/1745440806/p/1/e/6/t/1/d/hd/47109870.mp4',
     post: 'https://gw.alicdn.com/i2/TB11mhgOpXXXXaLXVXXXXXXXXXX_.jpg',
-    pic: 'https://gw.alicdn.com/i2/TB1CP.5NXXXXXawXXXXXXXXXXXX_.jpg',
+    pic: '',
     type: 'auto',
     name: '最萌身高差',
     msg: '观看： 11100000+'
   },{
     url: 'http://cloud.video.taobao.com/play/u/1745440806/p/1/e/6/t/1/d/hd/46833837.mp4',
     post: 'https://gw.alicdn.com/i4/TB1zwASNpXXXXaYXFXXXXXXXXXX_.jpg',
-    pic: 'https://gw.alicdn.com/i2/TB1CP.5NXXXXXawXXXXXXXXXXXX_.jpg',
+    pic: '',
     type: 'auto',
     name: '外公芳龄38',
     msg: '观看： 99600000+'

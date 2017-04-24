391798a5710ed48696c85eae3d4ed4edb963a614
Wave <372969383@qq.com>
2017-04-24 22:41:54 +0800

    application

---
+++ src/views/base.config.js
@@ -0,0 +1,16 @@
+// application content
+export const application = {
+  name: 'Wave',
+  location: '上海'
+}
+
+export const commonService = {
+  setTabbar:{
+    show: true,
+    tabbars: {
+      left: '电影',
+      center: '影院',
+      right: '我的'
+    }
+  }
+}

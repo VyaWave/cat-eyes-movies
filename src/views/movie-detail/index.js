26395bf600bc1d4799bbd2237aba0b8d623582c1
Wave <372969383@qq.com>
2017-05-09 10:14:35 +0800

    优化细节

--- src/views/movie-detail/index.js
+++ src/views/movie-detail/index.js
@@ -1,14 +1,23 @@
 import Component from 'vue-class-component'
 import{ BaseView } from '../base.view'
+import { LoadingIndicator } from '../../components/loading-indicator/index'
 import './style.scss'
 
 @Component({
-  template: require('./template.html')
+  template: require('./template.html'),
+  data: () => ({
+    isLoading: Boolean
+  }),
+  components: {LoadingIndicator},
 })
 
 export default class MovieDetail extends BaseView {
 
   created () {
+    this.isLoading = true
     this.headerConfig = { title: '影片详情' }
+    setTimeout(()=> {
+      this.isLoading = false
+    }, 1000 * 0.3)
   }
 }

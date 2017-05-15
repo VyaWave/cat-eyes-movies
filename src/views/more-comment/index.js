9fbe8cd3325c3eaa1d9469b64e6d6da414d00b86
Wave <372969383@qq.com>
2017-05-15 14:16:33 +0800

    something

---
+++ src/views/more-comment/index.js
@@ -0,0 +1,23 @@
+import Component from 'vue-class-component'
+import{ BaseView } from '../base.view'
+import { LoadingIndicator } from '../../components/loading-indicator/index'
+import './style.scss'
+
+@Component({
+  template: require('./template.html'),
+  data: () => ({
+    isLoading: Boolean
+  }),
+  components: {LoadingIndicator},
+})
+
+export default class MoreComment extends BaseView {
+
+  created () {
+    this.isLoading = true
+    this.headerConfig = { title: '影片评论' }
+    setTimeout(()=> {
+      this.isLoading = false
+    }, 1000 * 0.3)
+  }
+}
